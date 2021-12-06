672c4e59eda85ce79bdb3c43ab2f0a71414ffa92
1010Min <1010ally@naver.com>
2021-12-06 17:45:18 +0900

    PAINT JS 2: 그림판 저장하기

--- cloneCoding/paintjs/app.js
+++ cloneCoding/paintjs/app.js
@@ -4,6 +4,7 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 700;
@@ -12,6 +13,10 @@ const CANVAS_SIZE = 700;
 //만드는 것이므로 사이즈 지정이 필요
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
+
+//배경 이미지가 없으면 투명으로 저장되므로 미리 canvas 색 설정
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 
 ctx.strokeStyle = INITIAL_COLOR; //context 색상
 ctx.lineWidth = 2.5; //context 두께
@@ -74,12 +79,25 @@ function handleCanvasClick() {
     }
 }
 
+function handleCM(event) {
+    event.preventDefault(); //오른쪽 버튼 누르면 사진 저장 가능한 것 막기
+}
+
+function handleSaveClick() {
+    const image = canvas.toDataURL();
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS[🎨]"; //download는 anchor("a") 태그의 attribute
+    link.click();
+}
+
 if(canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting); //마우스를 클릭했을 때 발생하는 이벤트
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
     canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM);
 }
 
 Array.from(colors).forEach(color => 
@@ -92,3 +110,11 @@ if(range) {
 if(mode) {
     mode.addEventListener("click", handleModeClick);
 }
+
+if(saveBtn) {
+    saveBtn.addEventListener("click", handleSaveClick);
+}
+
+/* 
+Canvas API 홈페이지: https://developer.mozilla.org/ko/docs/Web/API/Canvas_API
+*/
