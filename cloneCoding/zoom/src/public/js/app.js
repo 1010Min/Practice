8604d3d3eadf190086d2bb56191aa58ffac77835
1010Min <1010ally@naver.com>
2021-12-08 15:25:25 +0900

    WebSocket: Frontend-Backend 연결 및 메세지 전송

--- cloneCoding/zoom/src/public/js/app.js
+++ cloneCoding/zoom/src/public/js/app.js
@@ -1,1 +1,22 @@
-alert("hi");
+//frontend와 backend 연결
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+//socket에 open 이벤트 추가
+socket.addEventListener("open", () => {
+    console.log("Connected to Server ✅");
+})
+
+//socket에 message 이벤트 추가
+socket.addEventListener("message", (message) => {
+    console.log("New message: ", message.data, " from the Server.")
+})
+
+//socket에 close 이벤트 추가
+socket.addEventListener("close", () => {
+    console.log("Disconnected from Server ❌");
+})
+
+//10초 뒤에메세지 보내기
+setTimeout(() => {
+    socket.send("hello from the browser!");
+}, 10000);
--- cloneCoding/zoom/src/server.js
+++ cloneCoding/zoom/src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -11,11 +13,27 @@ app.use("/public", express.static(__dirname + "/public"));
 
 //home으로 가면 request, response를 받고 res.render를 통해 만든 home을 render
 //home.pug를 render해주는 route handler 생성
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
 
 //유저가 어떤 url로 이동하던지 홈으로 돌려보냄(다른 url 사용하지 않을 것이므로)
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-app.listen(3000, handleListen);
+//createServer를 하려면 requestListener 경로가 있어야 함 -> application
+//express application으로부터 http 서버 만들기
+const server = http.createServer(app);
+
+//새로운 WebSocket 서버 만들기
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser ✅"); 
+    socket.on("close", () => console.log("Disconnected from the Browser ❌"))
+    socket.on("message", (message) => {
+        console.log(message.toString('utf8'));
+    })
+    socket.send("hello"); //서버에 있는 것이 아닌 socket에 있는 메서드
+}); //event가 발동하는 것 기다림
+
+server.listen(3000, handleListen);
