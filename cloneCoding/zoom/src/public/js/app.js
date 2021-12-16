0402897767280661f0a9d0ba78af19f362a8185a
1010Min <1010ally@naver.com>
2021-12-16 16:28:52 +0900

    Camera Switch: 카메라 바꾸기, 켜기/끄기, 음소거 등 기능

--- cloneCoding/zoom/src/public/js/app.js
+++ cloneCoding/zoom/src/public/js/app.js
@@ -1,83 +1,93 @@
-const socket = io(); //자동적으로 Back-end socket.io와 연결해주는 function
+const socket = io(); 
 
-const welcome = document.getElementById("welcome");
-const form = welcome.querySelector("form");
-const room = document.getElementById("room");
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
 
-room.hidden = true; //room 이름 입력하기 전에는 숨겨둠
+let myStream;
+let muted = false; //음소거 여부를 추적할 variable
+let cameraOff = false; //카메라가 켜져or꺼져 있는지 추적할 variable
 
-let roomName;
+async function getCameras() {
+    try {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter((device) => device.kind === "videoinput");
+        const currentCamera = myStream.getVideoTracks()[0];
+        cameras.forEach((camera) => {
+            const option = document.createElement("option");
+            option.value = camera.deviceId;
+            option.innerText = camera.label;
 
-function addMessage(message){//message를 추가해주는 function
-    const ul = room.querySelector("ul");
-    const li = document.createElement("li");
-    li.innerText = message;
-    ul.appendChild(li);
-}
+            if(currentCamera.label == camera.label){
+                option.selected = true;
+            }
 
-function handleMessageSubmit(event) { //Backend로 메세지 보냄
-    event.preventDefault();
-    const input = room.querySelector("#msg input");
-    const value = input.value;
-    socket.emit("new_message", input.value, roomName, () => {
-        addMessage(`You: ${value}`);
-    });
-    input.value = "";
-}
-
-function handleNicknameSubmit(event) { //Backend로 메세지 보냄
-    event.preventDefault();
-    const input = room.querySelector("#name input");
-    socket.emit("nickname", input.value);
-}
-
-function showRoom() {
-    welcome.hidden = true;
-    room.hidden = false;
-    const h3 = room.querySelector("h3");
-    h3.innerText = `Room ${roomName}`;
-
-    // welcome을 숨기고 room을 보여준 showRoom에서 form을 찾음
-    const msgForm = room.querySelector("#msg");
-    const nameForm = room.querySelector("#name");
-    msgForm.addEventListener("submit", handleMessageSubmit);
-    nameForm.addEventListener("submit", handleNicknameSubmit);
-}
-
-function handleRoomSubmit(event) {
-    event.preventDefault();
-    const input = form.querySelector("input");
-    socket.emit("enter_room", input.value, showRoom);
-    roomName = input.value; //참가자명 알려줌
-    input.value = "";
-}
-
-form.addEventListener("submit", handleRoomSubmit);
-
-
-socket.on("welcome", (user, newCount) => {
-    const h3 = room.querySelector("h3");
-    h3.innerText = `Room ${roomName} (${newCount})`;
-    addMessage(`${user} Joined!`);
-});
-
-socket.on("bye", (left, newCount) => {
-    const h3 = room.querySelector("h3");
-    h3.innerText = `Room ${roomName} (${newCount})`;
-    addMessage(`${left} Left!`);
-});
-
-socket.on("new_message", addMessage); //다른 서버로부터 메세지 받음
-
-socket.on("room_change", (rooms) => {
-    const roomList = welcome.querySelector("ul");
-    roomList.innerHTML = "";
-    if(rooms.length === 0){ //내 어플리케이션에 room이 하나도 없으면 비움
-        return;
+            camerasSelect.appendChild(option);
+        });
+    } catch(e) {
+        console.log(e);
     }
-    rooms.forEach(room => {
-        const li = document.createElement("li");
-        li.innerText = room;
-        roomList.append(li);
-    })
-});
+}
+
+async function getMedia(deviceId) {
+    const initialConstraints = {
+        audio: true, 
+        video: { facingMode: "user" }, 
+    }; //deviceId가 없이 getMedia를 호출했을 때의 constraints
+
+    const cameraConstraints = {
+        audio: true,
+        video: { deviceId: { exact: deviceId } },
+    };
+
+    try {
+        myStream = await navigator.mediaDevices.getUserMedia(
+            deviceId ? cameraConstraints : initialConstraints
+        );
+        myFace.srcObject = myStream;
+
+        //만약 deviceID가 없다면 카메라를 가져옴
+        if(!deviceId) {
+            await getCameras();
+        }
+    } catch(e) {
+        console.log(e);
+    }
+}
+
+getMedia();
+
+function handleMuteClick() {
+    myStream.getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+
+    if(!muted) {
+        muteBtn.innerText = "Unmute";
+        muted = true;
+    } else {
+        muteBtn.innerText = "Mute";
+        muted = false;
+    }
+}
+
+function handleCameraClick() {
+    myStream.getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+
+    if(cameraOff) {
+        cameraBtn.innerText = "Turn Camera Off";
+        cameraOff = false;
+    } else {
+        cameraBtn.innerText = "Turn Camera On";
+        cameraOff = true;
+    }
+}
+
+async function handleCameraChange() {
+    await getMedia(camerasSelect.value);
+}
+
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
+camerasSelect.addEventListener("input", handleCameraChange);
--- cloneCoding/zoom/src/server.js
+++ cloneCoding/zoom/src/server.js
@@ -1,114 +1,17 @@
 import http from "http";
-//import WebSocket from "ws";
-import { Server } from "socket.io";
+import SocketIO from "socket.io";
 import express from "express";
-import { instrument } from "@socket.io/admin-ui";
-import { emit } from "process";
 
 const app = express();
 
-app.set('view engine', "pug"); //Pug로 view engine 설정
-app.set("views", __dirname + "/views"); //Express에 template 경로 지정
-
-//유저가 /public으로 가게 되면 __dirname + "/public" 폴더가 보이게 설정
-//public url을 생성해 유저에게 파일 공유
+app.set('view engine', "pug"); 
+app.set("views", __dirname + "/views"); 
 app.use("/public", express.static(__dirname + "/public"));
-
-//home으로 가면 request, response를 받고 res.render를 통해 만든 home을 render
-//home.pug를 render해주는 route handler 생성
 app.get("/", (_, res) => res.render("home"));
-
-//유저가 어떤 url로 이동하던지 홈으로 돌려보냄(다른 url 사용하지 않을 것이므로)
 app.get("/*", (_, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-
-//createServer를 하려면 requestListener 경로가 있어야 함 -> application
-//express application으로부터 http 서버 만들기
 const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
-const wsServer = new Server(httpServer, {
-    cors: {
-        origin: ["https://admin.socket.io"],
-        credentials: true
-    }
-});
-
-instrument(wsServer, {
-    auth: false
-});
-
-function publicRooms() {
-    //sockets 안으로 들어가서 adapter를 갖고 sids와 rooms를 가져옴 -> wsServer 안에서
-    const {
-        sockets: {
-            adapter: {sids, rooms},
-        },
-    } = wsServer;
-
-    const publicRooms = [];
-    rooms.forEach((_, key) => {
-        if (sids.get(key) === undefined){
-            publicRooms.push(key);
-        }
-    });
-    return publicRooms;
-}
-
-function countRoom(roomName){
-    // 가끔 roomName을 찾을 수도 있지만 아닐수도 있을 때 ? 사용
-    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-}
-
-wsServer.on("connection", (socket) => {
-    socket["nickname"] = "Anon";
-    socket.onAny((event) => {
-        console.log(wsServer.sockets.adapter);
-        console.log(`Socket Event: ${event}`);
-    });
-    socket.on("enter_room", (roomName, done) => {
-        console.log(socket.id);
-        console.log(socket.rooms);
-        socket.join(roomName);
-        done();
-        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
-        wsServer.sockets.emit("room_change", publicRooms());
-    });
-    socket.on("disconnecting", () => {
-        socket.rooms.forEach((room) => 
-            //disconnecting은 방 퇴장 직전이므로 나가기 전의 나도 포함되서 계산됨 -> ∴ -1을 해줌
-            socket.to(room).emit("bye", socket.nickname, countRoom(room)-1));
-    });
-    socket.on("disconnect", () => {
-        wsServer.sockets.emit("room_change", publicRooms());
-    });
-    socket.on("new_message", (msg, room, done) => {
-        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
-        done();
-    });
-    socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
-});
-
-// //새로운 WebSocket 서버 만들기
-// const wss = new WebSocket.Server({ server });
-
-// const sockets = []; //누군가 서버에 연결하면 connection을 추가
-
-// wss.on("connection", (socket) => {
-//     sockets.push(socket);
-//     socket["nickname"] = "Anon";
-//     console.log("Connected to Browser ✅"); 
-//     socket.on("close", () => console.log("Disconnected from the Browser ❌"))
-//     socket.on("message", (msg) => {
-//         const message = JSON.parse(msg);
-//         switch(message.type){
-//             case "new_message":
-//                 //각 브라우저를 aSocket으로 표시하고 메세지를 보내는 것
-//                 sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));    
-//             case "nickname":
-//                 socket["nickname"] = message.payload;
-//         }
-//     });
-// }); //event가 발동하는 것 기다림
-
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
