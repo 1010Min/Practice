89a71fc19a4dafabab3cc289be467e4b548425e7
1010Min <1010ally@naver.com>
2023-01-18 11:38:16 +0900

    [ADD] 코딩테스트 입문 D-4

---
+++ programmers_python/coding_test_intro/day04_014.py
@@ -0,0 +1,14 @@
+import math
+
+def lcm(a,b):
+  return (a * b) // math.gcd(a,b)
+
+def solution(n):
+    answer = 0
+    
+    if(n % 6 == 0):
+        answer = n / 6
+    else:
+        answer = lcm(n, 6) / 6
+    
+    return answer
